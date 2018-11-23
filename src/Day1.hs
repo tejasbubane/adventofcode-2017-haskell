569af1d4cf8965bf7eb24ebcba9df5da11b269fa
Tejas Bubane <tejasbubane@gmail.com>
2018-11-23 20:26:51 +0530

    Complete day1 - part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,8 +1,15 @@
-module Day1 (captcha) where
+module Day1 (captcha1, captcha2) where
 
 import Data.Char (digitToInt)
 
-captcha :: String -> Int
-captcha xs = sum matchings where
-  matchings = map (digitToInt . fst) $ filter (uncurry (==)) zipAdjacent
+captcha1 :: String -> Int
+captcha1 xs = sum $ matchings zipAdjacent where
   zipAdjacent = zip xs $ (tail xs ++ [head xs])
+
+captcha2 :: String -> Int
+captcha2 xs = sum $ matchings zipped where
+  (fstHalf, sndHalf) = splitAt ((length xs) `div` 2) xs
+  zipped = zip xs $ sndHalf ++ fstHalf
+
+matchings :: [(Char, Char)] -> [Int]
+matchings xs = map (digitToInt . fst) $ filter (uncurry (==)) xs
