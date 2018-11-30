9b19d5a1e5e46ddcbc4f551b2995e83c8cec1ded
Tejas Bubane <tejasbubane@gmail.com>
2018-11-30 23:58:35 +0530

    Solve day6 - part 2

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,25 +1,29 @@
 module Day6 where
 
 import Data.Array
-import Data.Set
+import Data.HashMap hiding (elems)
 import Data.List (elemIndex)
 import Data.Maybe (fromMaybe)
 
 type Banks = Array Int Int
 
-reallocate :: Int -> Int -> Int -> Array Int Int -> Array Int Int
+reallocate :: Int -> Int -> Int -> Banks -> Banks
 reallocate _ _ 0 acc = acc
 reallocate idx limit balLeft acc =
   let newIdx = if idx + 1 > limit then 1 else idx + 1
+      l = (Data.Array.!)
   in
-    reallocate newIdx limit (balLeft - 1) (acc//[(newIdx, acc!newIdx + 1)])
+    reallocate newIdx limit (balLeft - 1) (acc//[(newIdx, acc`l`newIdx + 1)])
 
-balance :: Banks -> Set Banks -> Int
-balance banks acc =
+checkLoop :: Banks -> Map [Int] Int -> (Int, Int)
+checkLoop banks acc =
   let maxEl  = maximum banks
-      maxIdx = fromMaybe 0 $ (+1) <$> (elemIndex maxEl $ Data.Array.elems banks)
+      bankVals = elems banks
+      maxIdx = fromMaybe 0 $ (+1) <$> (elemIndex maxEl $ bankVals)
       newBanks = reallocate maxIdx (length banks) maxEl (banks//[(maxIdx, 0)])
+      steps = size acc
+      l = (Data.HashMap.!)
   in
-    if newBanks `member` acc
-    then (size acc) + 1
-    else balance newBanks $ insert newBanks acc
+    if bankVals `member` acc
+    then (steps, steps - acc`l`bankVals)
+    else checkLoop newBanks $ insert bankVals steps acc
--- test/Day6Spec.hs
+++ test/Day6Spec.hs
@@ -2,7 +2,7 @@ module Day6Spec where
 
 import Test.Hspec
 import Day6
-import Data.Set
+import Data.HashMap
 import Data.Array
 
 exampleInput :: Array Int Int
@@ -13,8 +13,14 @@ puzzleInput = listArray (1, 16) [10, 3, 15, 10, 5, 15, 5, 15, 9, 2, 5, 8, 5, 2, 
 
 specs :: SpecWith ()
 specs = describe "Day 6" $ do
-  describe "part 1" $ do
+  describe "part 1 - steps to reach loop" $ do
     it "works for example input" $ do
-      balance exampleInput empty `shouldBe` 5
+      fst (checkLoop exampleInput empty) `shouldBe` 5
     it "works for puzzle input" $ do
-      balance puzzleInput empty `shouldBe` 14029
+      fst (checkLoop puzzleInput empty) `shouldBe` 14029
+
+  describe "part 2 - loop cycle length" $ do
+    it "works for example input" $ do
+      snd (checkLoop exampleInput empty) `shouldBe` 4
+    it "works for puzzle input" $ do
+      snd (checkLoop puzzleInput empty) `shouldBe` 2765
